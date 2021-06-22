d8699a11b4fc4904f3091dcca438f9034804ed95
radzymin <yonatan22195@gmail.com>
2021-06-22 16:49:02 +0300

    first commit

--- index.js
+++ index.js
@@ -26,24 +26,26 @@ const ModelCarpentry = new schema({
         type: String,
         required: true
     },
-    type_tree: {
+    adress: {
+        type: {
+            street: String,
+            num: Number,
+            city: String,
+        },
+        required: true
+    },
+    fon_numbers: {
+        type: String,
+        required: true
+    },
+    types_of_woodens: {
         type: [{
-            name_of_Wooden_beam: {
-                type: String,
-                required: true
-            },
-            length: {
-                type: Number,
-                required: true
-            },
-            price: {
-                type: Number,
-                required: true
-            },
-            description: {
-                type: String,
-                required: false
-            }
+            name_of_Wooden_beam: String,
+            description: String,
+            price_and_existing_lengthes: [{
+                length: Number,
+                price: Number,
+            }],
         }],
         required: false
     }
@@ -58,7 +60,7 @@ app.post("/register", (req, res) => {
     var newCarpentry = new Carpentry(req.body);
     newCarpentry.save((err, newDoc) => {
         if (err) {
-            console.error("there is an error in user while sign up: "+err.message);
+            console.error("there is an error in user while sign up: " + err.message);
             return res.send(err);
         }
         console.log(newDoc);
@@ -100,17 +102,17 @@ app.put("/update", (req, res) => {
 
 app.get("/getAllCarpentry", (req, res) => {
     Carpentry.find((err, documents) => {
-        if(err)
-            return res.status(500).send(err); 
-        let type_tree_list=[];
-        let carpentry_shop_name_list=[];
-        for (let index = 0; index < documents.length; index++) {      
-            type_tree_list[index]=(documents[index].type_tree);
-            carpentry_shop_name_list[index]=(documents[index].carpentry_shop_name);
+        if (err)
+            return res.status(500).send(err);
+        let type_tree_list = [];
+        let carpentry_shop_name_list = [];
+        for (let index = 0; index < documents.length; index++) {
+            type_tree_list[index] = documents[index].type_tree;
+            carpentry_shop_name_list[index] = documents[index].carpentry_shop_name;
         }
         res.status(200).send({
-            type_tree_list:type_tree_list,
-            carpentry_shop_name_list:carpentry_shop_name_list
+            type_tree_list: type_tree_list,
+            carpentry_shop_name_list: carpentry_shop_name_list
         });
     });
 });
